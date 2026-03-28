ea0905b41db46aeeec47c39dbe973843a259f7a9
jessy! ✦ ݁˖ <jessyaj06@gmail.com>
2026-03-28 20:01:20 -0300

    Create numerosPares.java

---
+++ LaçoFor/numerosPares.java
@@ -0,0 +1,11 @@
+package prjLista3;
+
+public class NumPares {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 500; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " "); // imprime na mesma linha
+            }
+        }
+    }
+}
