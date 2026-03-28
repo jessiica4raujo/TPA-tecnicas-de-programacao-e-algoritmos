2b9df09cd0099674c63f73ce83342e53bfef97dc
jessy! ✦ ݁˖ <jessyaj06@gmail.com>
2026-03-28 19:55:45 -0300

    Update pesoPeixe.java

--- LaçoIfElse/pesoPeixe.java
+++ LaçoIfElse/pesoPeixe.java
@@ -1,31 +1,32 @@
-package PesoPeixe;
 import java.util.Scanner;
 
-public class Peixe{
+public class Peixe {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
 
-	double pesoPeixe, valorTotal, taxaAdicional=8.00;
-	
+        double pesoPeixe, valorTotal, taxaAdicional = 8.00;
 
-	System.out.println("Digite o peso em kg do peixe:");
-	pesoPeixe = in.nextDouble();
-	
-	if (pesoPeixe<3){
-	valorTotal< pesoPeixe *12 
-	System.out.println("Valor total a pagar: R$ " + String.format("%.2f", valorTotal));
-	}else if (pesoPeixe<5){
-	valorTotal< pesoPeixe *14 
-	System.out.println("Valor total a pagar: R$ " + String.format("%.2f", valorTotal));
+        System.out.println("Digite o peso em kg do peixe:");
+        pesoPeixe = in.nextDouble();
 
-	}else if (pesoPeixe<10){
-	valorTotal< pesoPeixe *15
-	System.out.println("Valor total a pagar: R$ " + String.format("%.2f", valorTotal));
-	
-	}else (pesoPeixe>10){
-	valorTotal< (pesoPeixe *17)+R$8,00 
-	System.out.println("Valor total a pagar: R$ " + String.format("%.2f", valorTotal));
-	 }
+        if (pesoPeixe < 3) {
+            valorTotal = pesoPeixe * 12;
+            System.out.println("Valor total a pagar: R$ " + valorTotal);
+
+        } else if (pesoPeixe < 5) {
+            valorTotal = pesoPeixe * 14;
+            System.out.println("Valor total a pagar: R$ " + valorTotal);
+
+        } else if (pesoPeixe < 10) {
+            valorTotal = pesoPeixe * 15;
+            System.out.println("Valor total a pagar: R$ " + valorTotal);
+
+        } else {
+            valorTotal = (pesoPeixe * 17) + taxaAdicional;
+            System.out.println("Valor total a pagar: R$ " + valorTotal);
+        }
+
         in.close();
-       }
-      }
+    }
+}
