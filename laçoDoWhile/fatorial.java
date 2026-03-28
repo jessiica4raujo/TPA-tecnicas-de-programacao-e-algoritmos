d4bbe040a535d802d3766b612818f3fb92770445
jessy! ✦ ݁˖ <jessyaj06@gmail.com>
2026-03-28 20:25:23 -0300

    Create fatorial.java

---
+++ laçoDoWhile/fatorial.java
@@ -0,0 +1,22 @@
+package prjTpa;
+import java.util.Scanner;
+public class Fatorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = 1;
+		int fatorial = 1;
+		int n;
+		System.out.println("Digite número para iniciar o fatorial:");
+		n = sc.nextInt();
+		
+	do {
+		fatorial = fatorial*i;
+		i++;
+	}while (i<= n);
+	
+	System.out.println("Fatorial de " + n +" é: ! " +fatorial);
+	}
+	
+		
+	}
